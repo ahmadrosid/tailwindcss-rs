ac79df7e5e26565c41a7df362f22efd6f7fce60d
ahmadrosid <alahmadrosid@gmail.com>
2022-03-25 17:29:51 +0700

    refactor: decouple write css function

--- src/generator/generate.rs
+++ src/generator/generate.rs
@@ -1,7 +1,7 @@
 use log::warn;
 
 use crate::config::Config;
-use crate::generator::Css;
+use crate::generator::{Buffer, Css};
 
 use std::collections::HashSet;
 use std::io::Read;
@@ -17,6 +17,43 @@ fn handle_prefix(line: &str, prefix: &str) -> Option<String> {
     }
 }
 
+pub fn write_css(buffer: Box<dyn Buffer>, config: Config, source: &HashSet<String>) {
+    let mut generator = Css::new(buffer, config);
+    for line in source.iter() {
+        if line.starts_with("text-") {
+            generator.generate_font_size(line);
+            continue;
+        } else if line.starts_with("font-") {
+            generator.generate_font_weight(line);
+            continue;
+        }
+
+        if let Some(prefix) = handle_prefix(line, "leading") {
+            generator.generate_line_height(&prefix, line);
+            continue;
+        }
+
+        if handle_prefix(line, "bg").is_some() {
+            generator.generate_background_color(line);
+            continue;
+        }
+
+        if handle_prefix(line, "aspect").is_some() {
+            generator.generate_aspect_ratio(line);
+            continue;
+        }
+
+        if handle_prefix(line, "break").is_some() {
+            generator.generate_break_point(line);
+            continue;
+        }
+
+        if generator.generate_plugin(line).is_some() {
+            continue;
+        }
+    }
+}
+
 pub fn execute(source: &HashSet<String>, output: &str, config_json: &Config) {
     let css_file = std::fs::OpenOptions::new()
         .write(true)
@@ -28,49 +65,14 @@ pub fn execute(source: &HashSet<String>, output: &str, config_json: &Config) {
 
     match css_file {
         Ok(mut file) => {
-            let buffer = BufferWriter::new(file.try_clone().unwrap());
-            let mut generator = Css::new(Box::new(buffer), config_json.clone());
             if file.read_to_string(&mut data).is_err() {
                 warn!("Unable to read file: {}", output);
                 std::process::exit(1);
             }
 
-            for line in source.iter() {
-                if line.starts_with("text-") {
-                    generator.generate_font_size(line);
-                    continue;
-                } else if line.starts_with("font-") {
-                    generator.generate_font_weight(line);
-                    continue;
-                }
-
-                if let Some(prefix) = handle_prefix(line, "leading") {
-                    generator.generate_line_height(&prefix, line);
-                    continue;
-                }
-
-                if handle_prefix(line, "bg").is_some() {
-                    generator.generate_background_color(line);
-                    continue;
-                }
-
-                if handle_prefix(line, "aspect").is_some() {
-                    generator.generate_aspect_ratio(line);
-                    continue;
-                }
-
-                if handle_prefix(line, "break").is_some() {
-                    generator.generate_break_point(line);
-                    continue;
-                }
-
-                if generator.generate_plugin(line).is_some() {
-                    continue;
-                }
-            }
-        }
-        _ => {
-            warn!("Unable to read file: {}", output);
-        }
-    }
+            let buffer = BufferWriter::new(file.try_clone().unwrap());
+            write_css(Box::new(buffer), config_json.clone(), source)
+        },
+        _ => warn!("Unable to create file: {}", output)
+    };
 }
